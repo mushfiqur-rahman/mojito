27a89d085178c4062f484850ff684803e9747a7e
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-08-02 22:10:40 +0600

    hero animation done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from "./components/Navbar.jsx";
-// import Hero from "./components/Hero.jsx";
+import Hero from "./components/Hero.jsx";
 // import Cocktails from "./components/Cocktails.jsx";
 // import About from "./components/About.jsx";
 // import Art from "./components/Art.jsx";
@@ -15,8 +15,9 @@ const App = () => {
   return (
     <main>
       <Navbar />
-      {/* <Hero />
-      <Cocktails />
+      <Hero />
+
+      {/* <Cocktails />
       <About />
       <Art />
       <Menu />
