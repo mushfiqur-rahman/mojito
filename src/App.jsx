daa1a262429919946de211c6c5c336f7947a9f91
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-08-02 22:21:37 +0600

    cocktails section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
-// import Cocktails from "./components/Cocktails.jsx";
+import Cocktails from "./components/Cocktails.jsx";
 // import About from "./components/About.jsx";
 // import Art from "./components/Art.jsx";
 // import Menu from "./components/Menu.jsx";
@@ -16,9 +16,8 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-
-      {/* <Cocktails />
-      <About />
+      <Cocktails />
+      {/* <About />
       <Art />
       <Menu />
       <Contact /> */}
