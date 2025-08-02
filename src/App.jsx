87781e24ddc44dede0c8ef549a8351ed484a4671
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-08-02 22:39:04 +0600

    project complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
-// import About from "./components/About.jsx";
-// import Art from "./components/Art.jsx";
-// import Menu from "./components/Menu.jsx";
-// import Contact from "./components/Contact.jsx";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
+import Menu from "./components/Menu.jsx";
+import Contact from "./components/Contact.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -17,10 +17,10 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
-      {/* <About />
+      <About />
       <Art />
       <Menu />
-      <Contact /> */}
+      <Contact />
     </main>
   );
 };
